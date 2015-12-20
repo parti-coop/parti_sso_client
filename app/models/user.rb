5902f505935d37ff801008ae80e9293c396da300
dali <dali@ufofactory.org>
2015-12-20 16:33:25 +0900

    bugfix. nickname should be null if extra_attributes is empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   devise :cas_authenticatable
 
   def cas_extra_attributes=(extra_attributes)
-    self.nickname = (extra_attributes["nickname"] || self.nickname)
+    self.nickname = (extra_attributes.try(:[], "nickname") || self.nickname)
   end
 end
