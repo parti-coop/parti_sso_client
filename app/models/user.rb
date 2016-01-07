5ec9ee698416fbca2200e198ddc97845b3516a5a
dali <dali@ufofactory.org>
2016-01-07 18:36:31 +0900

    refactoring sync

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
     self.nickname = (extra_attributes.try(:[], "nickname") || self.nickname)
   end
 
-  def self.sync(key)
-    url = "#{URI.join(Devise.cas_base_url, 'users/fetch.json')}?#{{key: key}.to_query}"
+  def self.sync(params)
+    url = "#{URI.join(Devise.cas_base_url, 'users/fetch.json')}?#{params.to_query}"
     result = JSON.parse(open(url).read)
     user = User.find_or_initialize_by email: result["username"]
     user.cas_extra_attributes = result["extra_attributes"]
@@ -18,6 +18,14 @@ class User < ActiveRecord::Base
   rescue => e
     logger.error e
     nil
+  end
+
+  def self.find_or_sync(user_key)
+    self.find_by(nickname: user_key) || self.find_by(email: user_key) || self.sync(key: user_key)
+  end
+
+  def self.find_or_sync_by_nickname(nickname)
+    self.find_by(nickname: nickname) || self.sync(nickname: nickname)
   end
 
   def image_url
